504f432705d4c6c2813b51b1295c0323d882d1d7
Nannapat-p <nannapat.pak@ku.th>
2025-08-08 15:29:00 +0700

    Add files via upload

--- Lib/InvalidOperationException.java
+++ Lib/InvalidOperationException.java
@@ -1,5 +1,9 @@
 package Lib;
 
+
+/**
+ * จะโยน Exception ก็ต่อเมื่อลบสินค้าไม่สำเร็จ
+ */
 public class InvalidOperationException extends Exception {
     public InvalidOperationException(String message){
         super(message);
--- Lib/ProductNotFoundException.java
+++ Lib/ProductNotFoundException.java
@@ -1,5 +1,10 @@
 package Lib;
 
+
+
+/**
+ * จะโยน Exception ออกไปเมื่อหาสินค้าไม่เจอใน ID
+ */
 public class ProductNotFoundException extends Exception {
     public  ProductNotFoundException(String message){
         super(message);
